d65e04bc58513c719228d5b3e725a1046e175e5e
Aiden <42216064+ChangeYu0229@users.noreply.github.com>
2023-09-02 09:39:37 -0400

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,6 +1,6 @@
 export const OWNER = "AI";
 export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://chatfree.aigc369.com`;
+export const REPO_URL = ``;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const RELEASE_URL = `${REPO_URL}/releases`;
