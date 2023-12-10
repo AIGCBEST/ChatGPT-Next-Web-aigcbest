31dd157d9e4a5d6abdaaefb4a18d2cc014fa9541
Aiden <42216064+ChangeYu0229@users.noreply.github.com>
2023-12-10 16:29:19 -0500

    1

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -16,9 +16,9 @@ import { copyToClipboard, downloadAs, useMobileScreen } from "../utils";
 
 import CopyIcon from "../icons/copy.svg";
 import LoadingIcon from "../icons/three-dots.svg";
-import ChatGptIcon from "../icons/chatgpt.png";
+import ChatGptIcon from "../icons/chatgpt1.png";
 import ShareIcon from "../icons/share.svg";
-import BotIcon from "../icons/bot.png";
+import BotIcon from "../icons/bot1.png";
 
 import DownloadIcon from "../icons/download.svg";
 import { useEffect, useMemo, useRef, useState } from "react";
